fcc02f4c4c0c300728c6efa572d33adaa751eeed
Darren Chang <darren@tripla.jp>
2021-10-21 21:15:32 +0900

    Update welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      redirect_to new_game_url
+    else
+      redirect_to new_user_url
+    end
   end
 end
---
+++ spec/requests/welcome_spec.rb
@@ -0,0 +1,24 @@
+describe '/', type: :request do
+  describe 'GET /' do
+    context 'when there is a user session' do
+      let!(:user) { create(:user) }
+
+      before do
+        allow_any_instance_of(ActionDispatch::Request)
+          .to receive(:session).and_return({ current_user_id: user.id })
+      end
+
+      it 'redirects to new game url' do
+        get root_url
+        expect(response).to redirect_to(new_game_url)
+      end
+    end
+
+    context 'when there is no user session' do
+      it 'redirects to new user url' do
+        get root_url
+        expect(response).to redirect_to(new_user_url)
+      end
+    end
+  end
+end
