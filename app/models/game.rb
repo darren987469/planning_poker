2d94f4be3018e04e92d6867518e02059ee6203e4
Darren Chang <darren@tripla.jp>
2021-10-23 17:43:05 +0900

    Add validation for game statu change

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,14 @@ class Game < ApplicationRecord
 
   enum status: { not_started: 'not_started', voting: 'voting', voting_ended: 'voting_ended' }
 
+  STATUS_CHANGE_MAP = {
+    'not_started' => 'voting',
+    'voting' => 'voting_ended',
+    'voting_ended' => 'voting'
+  }.freeze
+
   validates :name, presence: true
+  validate :validate_status_change, if: -> { status_changed? }
 
   before_create :set_code
 
@@ -19,5 +26,12 @@ class Game < ApplicationRecord
       random_code = SecureRandom.hex(10)
       break random_code unless Game.where(code: random_code).exists?
     end
+  end
+
+  def validate_status_change
+    valid_status = STATUS_CHANGE_MAP[status_was]
+    return if status == valid_status
+
+    errors.add(:base, 'invalid game status change')
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,4 +8,10 @@ describe Game, type: :model do
     create(:game, code: 'code')
     expect { create(:game, code: 'code') }.to raise_error ActiveRecord::RecordNotUnique
   end
+
+  it 'returns error when invalid status change' do
+    game = build(:game, status: 'voting_ended')
+    expect(game).to be_invalid
+    expect(game.errors.full_messages).to eq ['invalid game status change']
+  end
 end
